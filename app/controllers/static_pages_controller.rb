7dd7d9ebffb00a8a3cd791549ba1d42f503a7271
rmandavilli <raj_mandavilli@yahoo.com>
2014-06-04 17:43:14 -0500

    Add LoanPrepay pages; Add app icons to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -20,4 +20,7 @@ class StaticPagesController < ApplicationController
   def houseBuyRent
   end
 
+  def loanPrepay
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Usrcube::Application.routes.draw do
   match '/apps',                  to: 'static_pages#apps',              via: 'get'
   match '/apps/houseLoan',        to: 'static_pages#houseLoan',         via: 'get'
   match '/apps/houseBuyRent',     to: 'static_pages#houseBuyRent',      via: 'get'
+  match '/apps/loanPrepay',       to: 'static_pages#loanPrepay',        via: 'get'
 
   #match '/support',     to: 'static_pages#support',     via: 'get'
   #match '/support',     to: 'static_pages#support',     via: 'post'
